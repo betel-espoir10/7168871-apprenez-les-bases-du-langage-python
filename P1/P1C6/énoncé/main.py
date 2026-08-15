b495522ad93ac05ebe7fab2db77a022ecc0f9f68
betel_espoir10 <espoirgonbeloum@gmail.com>
2026-08-15 16:11:54 +0200

    P1-Exercice4

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,24 @@
 # Écrivez votre code ici !
+
+#Creation de la liste fruits
+fruits = ["pomme", "banane", "orange"]
+print(fruits)
+
+#Ajout de kiwi
+fruits.append("kiwi")
+print(fruits)
+
+#suppression de orange dans la liste
+fruits.remove("orange")
+print(fruits)
+
+#Modification de banane en ananas
+fruits[1] = "ananas"
+print(fruits)
+
+#Affichage de la taille de la liste
+print(len(fruits))
+
+#Tri de la liste par ordre alphabetique
+fruits.sort()
+print(fruits)
