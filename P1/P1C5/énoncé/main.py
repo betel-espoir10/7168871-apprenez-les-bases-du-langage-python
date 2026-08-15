48b1d3f46baf8e4e2ef7da63dd9c65197bd54f19
betel_espoir10 <espoirgonbeloum@gmail.com>
2026-08-15 15:33:44 +0200

    P1-Exercice3

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,21 @@
 # Écrivez votre code ici !
+
+#Declarations des variables
+nom = "PythonBoy"
+age = 25
+taille = 1.87
+est_etudiant = True
+
+#Affichage de la valeur des variables
+print(f"Nom: {nom}")
+print(f"Age: {age}")
+print(f"Taille: {taille}")
+print(f"Est_etudiant: {est_etudiant}")
+
+print("*********************************")
+#Affichage du type des variables
+
+print(f"Type Nom: {type(nom)}")
+print(f"Type Nom: {type(age)}")
+print(f"Type Nom: {type(taille)}")
+print(f"Type Nom: {type(est_etudiant)}")
