5e61e48b1395938ba80046120b3967b4d474c2c5
betel_espoir10 <espoirgonbeloum@gmail.com>
2026-08-15 14:55:19 +0200

    P1-Exercice2

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,14 @@
 # Écrivez votre code ici !
+
+#declaration des variables
+
+nom = "PythonBoy"
+age = 25
+
+#Affichage du texte contenant le nom et l'age
+print(f" je m'appelle {nom} et j'ai {age} ans, j'apprends le langage de programmation Python")
+
+print("******************************")
+#Modification de l'age et affichage
+age = 30
+print(f" je m'appelle {nom} et j'ai {age} ans, j'apprends le langage de programmation Python")
